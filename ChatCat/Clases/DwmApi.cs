a35d42cdd138c0b5748c1405e45796670a1af68b
Timmy <nachodortta@hotmail.com>
2025-09-02 06:02:32 -0300

    Nueva clase "DwmApi"
    Esto me permitirá poder hacer que el titlebar de las ventanas tengan soporte al modo oscuro de Windows 10/11

---
+++ ChatCat/Clases/DwmApi.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+
+public static class DwmApi
+{
+    [DllImport("dwmapi.dll", PreserveSig = true)]
+    public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+    // DWMWA_USE_IMMERSIVE_DARK_MODE attribute value
+    // Use 19 for Windows versions before 20H1 (18985)
+    // Use 20 for Windows versions 20H1 (18985) and later
+    private const int DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1 = 19;
+    private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+
+    public static bool UseImmersiveDarkMode(IntPtr handle, bool enabled)
+    {
+        if (OperatingSystem.IsWindowsVersionAtLeast(10, 0, 17763)) // Windows 10 October 2018 Update or later
+        {
+            var attribute = DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1;
+            if (OperatingSystem.IsWindowsVersionAtLeast(10, 0, 18985)) // Windows 10 20H1 Update or later
+            {
+                attribute = DWMWA_USE_IMMERSIVE_DARK_MODE;
+            }
+
+            int useImmersiveDarkMode = enabled ? 1 : 0;
+            return DwmSetWindowAttribute(handle, attribute, ref useImmersiveDarkMode, sizeof(int)) == 0;
+        }
+        return false;
+    }
+}
